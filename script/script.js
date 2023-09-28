b1432d8f644e1f8093ee1e5c33a2925fdc08d676
rohit.jeerh <rohit.jeerh@RJEERH>
2023-09-28 14:58:03 +0800

    Adjust photo resolution and alignment of info text

--- script/script.js
+++ script/script.js
@@ -13,4 +13,15 @@ function typeWriter() {
     setTimeout(typeWriter, 20);
   }
 }
-
+/* var j=0;
+var title = document.getElementById("title").innerHTML;
+var whtml = document.getElementById("title");
+document.getElementById("title").innerHTML = "";
+function position() {
+  if (j < title.length) {
+    whtml.innerHTML += title.charAt(i);
+    i++;
+    setTimeout(position, 20);
+  }
+}
+*/
